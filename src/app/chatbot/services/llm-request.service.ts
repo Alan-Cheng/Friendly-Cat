b8fce96c2bd63e928e177578ff814f9309271f3b
PCS\AlanJHCheng <AlanJHCheng@taiwanmobile.com>
2025-10-30 10:06:35 +0800

    換模型，免費的grok不見ㄌ

--- src/app/chatbot/services/llm-request.service.ts
+++ src/app/chatbot/services/llm-request.service.ts
@@ -8,7 +8,7 @@ import { Observable, switchMap } from 'rxjs';
 export class LlmRequestService {
 
   url = "https://openrouter.ai/api/v1/chat/completions";
-  model = "x-ai/grok-4-fast:free";
+  model = "deepseek/deepseek-chat-v3.1:free";
   
   constructor(
     private requestService: RequestService
